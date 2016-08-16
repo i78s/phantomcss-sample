b745c97c6de1453d81e8b4c283b83b278243a738
稲葉 聡 <inaba182@gmail.com>
2016-08-16 15:46:26 +0900

    first commit

---
+++ test.js
@@ -0,0 +1,36 @@
+'use strict';
+
+var phantomcss = require('phantomcss');
+
+// start a casper test
+casper.test.begin('Tags', function(test) {
+
+    phantomcss.init({
+        rebase: casper.cli.get('rebase')
+    });
+
+    // open page
+    casper.start('https://liginc.co.jp/');
+
+    // set your preferred view port size
+    casper.viewport(1024, 768);
+
+    casper.then(function() {
+        // take the screenshot of the whole body element and save it under "body.png". The first parameter is actually a CSS selector
+        phantomcss.screenshot('.l-header', 'header');
+    });
+
+    casper.then(function now_check_the_screenshots() {
+        // compare screenshots
+        phantomcss.compareAll();
+    });
+
+    // run tests
+    casper.run(function() {
+        console.log('\nTHE END.');
+        casper.test.done();
+    });
+});
+
+
+
